4349b214d7345ae087f6723ab25004dbe67951e3
Leonardo Abreu <leonardoavelinoabreu@gmail.com>
2026-06-05 12:51:55 -0300

    working in the css of the project

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+//api key is in .env file
 const apiKey = import.meta.env.VITE_API_KEY;
 
+//this part of the url is the same for all requests
 const url = "https://api.openweathermap.org/data/2.5/weather?q=";
 
+/* Waits for the entire page to load, then find the search form.
+  Every time the form is submitted, don't reload the page, instead run onSearchCity.*/
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("searchForm").addEventListener("submit", (e) => {
     e.preventDefault();
@@ -12,7 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
 function onSearchCity() {
   const city = document.getElementById("citySearch").value;
   document.getElementById("citySearch").value = "";
-  //console.log(city);
   document.getElementById("location").textContent = `${city.toUpperCase()}`;
   getWeather(city);
 }
@@ -27,7 +30,7 @@ async function getWeather(city) {
       `${url}${encodeURIComponent(city)}&appid=${apiKey}&units=metric`,
     );
     const data = await response.json();
-    document.getElementById("temperature").textContent = data.main.temp;
+    document.getElementById("temperature").textContent = `${data.main.temp}°C`;
     document.getElementById("weatherCondition").textContent =
       data.weather[0].description;
   } catch (error) {
